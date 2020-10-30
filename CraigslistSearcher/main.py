eeebdf77aa6aae9dfa0eb8ac3a884fe1e0b0ad69
connorholm <connorjholm@gmail.com>
2020-10-30 09:16:37 -0500

    Adding a info array with the url and title

--- CraigslistSearcher/main.py
+++ CraigslistSearcher/main.py
@@ -8,13 +8,28 @@ def getProductList():
     soup = BeautifulSoup(page.text, "html.parser")
     #User puts in a word, program returns items of the word that are free
     titlesHTML = soup.find_all(class_ = "result-heading")
+    titlesURL = soup.find_all(class_="result-title hdrlnk")
     titles = []
+    sources = []
     correctTitle = ""
-    titleLength = 0
+    info = []
+    index = 0
+
+    for link in titlesURL:
+        sources.append(link['href'])
+
     for title in titlesHTML:
         correctTitle = title.get_text()
         titles.append(correctTitle[1:len(correctTitle)-1:])
-    print(titles)
-
+    
+    for _ in titles:
+        info.append(
+            {
+                "title":titles[index],
+                "sources":sources[index]
+            }
+        )
+        index+=1
+    print(info)
 if __name__ == "__main__":
     getProductList()
